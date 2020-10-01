3ca5394110e05c3172624b868a22252e7a4de9dd
Soffan Marsus Ahmad <soffanma@gmail.com>
2020-10-01 21:01:26 +0700

    First commit

---
+++ app/src/main/java/com/soffanma/pertemuan3mobile/Activity/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.soffanma.pertemuan3mobile.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.soffanma.pertemuan3mobile.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
